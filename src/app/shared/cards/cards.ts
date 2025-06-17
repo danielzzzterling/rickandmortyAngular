2f76768afd9a92ea1d89e88ab146d3c18ddd54bd
danielzzterling <rodriguezsterlingd@gmail.com>
2025-06-17 16:54:43 -0500

    commit de prueba para trabajar en casa

--- src/app/shared/cards/cards.ts
+++ src/app/shared/cards/cards.ts
@@ -1,13 +1,12 @@
 import { Component, Input, input} from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-cards',
   standalone:true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.html',
   styleUrl: './cards.scss'
 })
 export class Cards {
-  @Input() character: any;
-}
+  @Input() character!: any;}
