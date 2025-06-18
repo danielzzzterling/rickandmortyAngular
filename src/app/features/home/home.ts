a7422f646108be1ea124482aff1c1fce671ab4f3
Daniel <rodriguezsterlingd@gmail.com>
2025-06-18 00:13:33 -0500

    push end

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -4,6 +4,7 @@ import { Header } from '../../shared/header/header';
 import { Cards } from '../../shared/cards/cards';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- importante
+import { NavbarComponent } from '../../shared/navbar/navbar';
 
 @Component({
   selector: 'app-home',
